b229d4aa20dcef22e12bbd75aaf2da747a013201
bobby <cxiangyu@gmail.com>
2022-10-29 13:18:14 +0200

    Created a mockapp

---
+++ mockapp/src/App.js
@@ -0,0 +1,203 @@
+import React, { Component } from "react";
+import { Routes , Route, Link, BrowserRouter as Router } from "react-router-dom";
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+
+import AddProduct from './components/AddProduct';
+import Cart from './components/Cart';
+import Login from './components/Login';
+import ProductList from './components/ProductList';
+
+import Context from "./Context";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+      cart: {},
+      products: []
+    };
+    this.routerRef = React.createRef();
+  }
+
+  async componentDidMount() {
+    let user = localStorage.getItem("user");
+    let cart = localStorage.getItem("cart");
+
+    const products = await axios.get('http://localhost:3001/products');
+    user = user ? JSON.parse(user) : null;
+    cart = cart? JSON.parse(cart) : {};
+
+    this.setState({ user,  products: products.data, cart });
+  }
+
+  login = async (email, password) => {
+    const res = await axios.post(
+      'http://localhost:3001/login',
+      { email, password },
+    ).catch((res) => {
+      return { status: 401, message: 'Unauthorized' }
+    })
+
+    if(res.status === 200) {
+      const { email } = jwt_decode(res.data.accessToken)
+      const user = {
+        email,
+        token: res.data.accessToken,
+        accessLevel: email === 'admin@example.com' ? 0 : 1
+      }
+
+      this.setState({ user });
+      localStorage.setItem("user", JSON.stringify(user));
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  logout = e => {
+    e.preventDefault();
+    this.setState({ user: null });
+    localStorage.removeItem("user");
+  };
+
+  addProduct = (product, callback) => {
+    let products = this.state.products.slice();
+    products.push(product);
+    this.setState({ products }, () => callback && callback());
+  };
+
+  addToCart = cartItem => {
+    let cart = this.state.cart;
+    if (cart[cartItem.id]) {
+      cart[cartItem.id].amount += cartItem.amount;
+    } else {
+      cart[cartItem.id] = cartItem;
+    }
+    if (cart[cartItem.id].amount > cart[cartItem.id].product.stock) {
+      cart[cartItem.id].amount = cart[cartItem.id].product.stock;
+    }
+    localStorage.setItem("cart", JSON.stringify(cart));
+    this.setState({ cart });
+  };
+
+  removeFromCart = cartItemId => {
+    let cart = this.state.cart;
+    delete cart[cartItemId];
+    localStorage.setItem("cart", JSON.stringify(cart));
+    this.setState({ cart });
+  };
+
+  clearCart = () => {
+    let cart = {};
+    localStorage.removeItem("cart");
+    this.setState({ cart });
+  };
+
+  checkout = () => {
+    if (!this.state.user) {
+      this.routerRef.current.history.push("/login");
+      return;
+    }
+
+    const cart = this.state.cart;
+
+    const products = this.state.products.map(p => {
+      if (cart[p.name]) {
+        p.stock = p.stock - cart[p.name].amount;
+
+        axios.put(
+          `http://localhost:3001/products/${p.id}`,
+          { ...p },
+        )
+      }
+      return p;
+    });
+
+    this.setState({ products });
+    this.clearCart();
+  };
+
+  render() {
+    return (
+      <Context.Provider
+        value={{
+          ...this.state,
+          removeFromCart: this.removeFromCart,
+          addToCart: this.addToCart,
+          login: this.login,
+          addProduct: this.addProduct,
+          clearCart: this.clearCart,
+          checkout: this.checkout
+        }}
+      >
+        <Router ref={this.routerRef}>
+        <div className="App">
+          <nav
+            className="navbar container"
+            role="navigation"
+            aria-label="main navigation"
+          >
+            <div className="navbar-brand">
+              <b className="navbar-item is-size-4 ">ecommerce</b>
+              <label
+                role="button"
+                class="navbar-burger burger"
+                aria-label="menu"
+                aria-expanded="false"
+                data-target="navbarBasicExample"
+                onClick={e => {
+                  e.preventDefault();
+                  this.setState({ showMenu: !this.state.showMenu });
+                }}
+              >
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+              </label>
+            </div>
+              <div className={`navbar-menu ${
+                  this.state.showMenu ? "is-active" : ""
+                }`}>
+                <Link to="/products" className="navbar-item">
+                  Products
+                </Link>
+                {this.state.user && this.state.user.accessLevel < 1 && (
+                  <Link to="/add-product" className="navbar-item">
+                    Add Product
+                  </Link>
+                )}
+                <Link to="/cart" className="navbar-item">
+                  Cart
+                  <span
+                    className="tag is-primary"
+                    style={{ marginLeft: "5px" }}
+                  >
+                    { Object.keys(this.state.cart).length }
+                  </span>
+                </Link>
+                {!this.state.user ? (
+                  <Link to="/login" className="navbar-item">
+                    Login
+                  </Link>
+                ) : (
+                  <Link to="/" onClick={this.logout} className="navbar-item">
+                    Logout
+                  </Link>
+                )}
+              </div>
+            </nav>
+            <Routes>
+              <Route exact path="/" component={ProductList} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/cart" component={Cart} />
+              <Route exact path="/add-product" component={AddProduct} />
+              <Route exact path="/products" component={ProductList} />
+            </Routes>
+          </div>
+        </Router>
+      </Context.Provider>
+    );
+  }
+}
---
+++ mockapp/src/components/AddProduct.js
@@ -0,0 +1,149 @@
+import React, { Component } from "react";
+import withContext from "../withContext";
+import { Navigate } from "react-router-dom";
+import axios from 'axios';
+
+const initState = {
+  name: "",
+  price: "",
+  stock: "",
+  shortDesc: "",
+  description: ""
+};
+
+class AddProduct extends Component {
+  constructor(props) {
+    super(props);
+    this.state = initState;
+  }
+
+  save = async (e) => {
+    e.preventDefault();
+    const { name, price, stock, shortDesc, description } = this.state;
+
+    if (name && price) {
+      const id = Math.random().toString(36).substring(2) + Date.now().toString(36);
+
+      await axios.post(
+        'http://localhost:3001/products',
+        { id, name, price, stock, shortDesc, description },
+      )
+
+      this.props.context.addProduct(
+        {
+          name,
+          price,
+          shortDesc,
+          description,
+          stock: stock || 0
+        },
+        () => this.setState(initState)
+      );
+      this.setState(
+        { flash: { status: 'is-success', msg: 'Product created successfully' }}
+      );
+
+    } else {
+      this.setState(
+        { flash: { status: 'is-danger', msg: 'Please enter name and price' }}
+      );
+    }
+  };
+
+  handleChange = e => this.setState({ [e.target.name]: e.target.value, error: "" });
+
+  render() {
+    const { name, price, stock, shortDesc, description } = this.state;
+    const { user } = this.props.context;
+
+    return !(user && user.accessLevel < 1) ? (
+      <Navigate to="/" />
+    ) : (
+      <>
+        <div className="hero is-primary ">
+          <div className="hero-body container">
+            <h4 className="title">Add Product</h4>
+          </div>
+        </div>
+        <br />
+        <br />
+        <form onSubmit={this.save}>
+          <div className="columns is-mobile is-centered">
+            <div className="column is-one-third">
+              <div className="field">
+                <label className="label">Product Name: </label>
+                <input
+                  className="input"
+                  type="text"
+                  name="name"
+                  value={name}
+                  onChange={this.handleChange}
+                  required
+                />
+              </div>
+              <div className="field">
+                <label className="label">Price: </label>
+                <input
+                  className="input"
+                  type="number"
+                  name="price"
+                  value={price}
+                  onChange={this.handleChange}
+                  required
+                />
+              </div>
+              <div className="field">
+                <label className="label">Available in Stock: </label>
+                <input
+                  className="input"
+                  type="number"
+                  name="stock"
+                  value={stock}
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="field">
+                <label className="label">Short Description: </label>
+                <input
+                  className="input"
+                  type="text"
+                  name="shortDesc"
+                  value={shortDesc}
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="field">
+                <label className="label">Description: </label>
+                <textarea
+                  className="textarea"
+                  type="text"
+                  rows="2"
+                  style={{ resize: "none" }}
+                  name="description"
+                  value={description}
+                  onChange={this.handleChange}
+                />
+              </div>
+              {this.state.flash && (
+                <div className={`notification ${this.state.flash.status}`}>
+                  {this.state.flash.msg}
+                </div>
+              )}
+              <div className="field is-clearfix">
+                <button
+                  className="button is-primary is-outlined is-pulled-right"
+                  type="submit"
+                  onClick={this.save}
+                >
+                  Submit
+                </button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </>
+    );
+  }
+}
+
+export default withContext(AddProduct);
