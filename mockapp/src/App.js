ccc5ed7c52737e781c70a6e93f394c76467eaebc
bobby <cxiangyu@gmail.com>
2022-11-01 15:25:30 +0100

    updated the rendering in App.js

--- mockapp/src/App.js
+++ mockapp/src/App.js
@@ -10,7 +10,7 @@ import ProductList from './components/ProductList';
 
 import Context from "./Context";
 
-export default class App extends Component {
+export default class App extends Component{
   constructor(props) {
     super(props);
     this.state = {
@@ -121,83 +121,83 @@ export default class App extends Component {
 
   render() {
     return (
-      <Context.Provider
-        value={{
-          ...this.state,
-          removeFromCart: this.removeFromCart,
-          addToCart: this.addToCart,
-          login: this.login,
-          addProduct: this.addProduct,
-          clearCart: this.clearCart,
-          checkout: this.checkout
-        }}
-      >
-        <Router innerRef={this.routerRef}>
-        <div className="App">
-          <nav
-            className="navbar container"
-            role="navigation"
-            aria-label="main navigation"
-          >
-            <div className="navbar-brand">
-              <b className="navbar-item is-size-4 ">mockapp</b>
-              <label
-                role="button"
-                className="navbar-burger burger"
-                aria-label="menu"
-                aria-expanded="false"
-                data-target="navbarBasicExample"
-                onClick={e => {
-                  e.preventDefault();
-                  this.setState({ showMenu: !this.state.showMenu });
-                }}
-              >
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-              </label>
-            </div>
-              <div className={`navbar-menu ${
-                  this.state.showMenu ? "is-active" : ""
-                }`}>
-                <Link to="/products" className="navbar-item">
-                  Products
-                </Link>
-                {this.state.user && this.state.user.accessLevel < 1 && (
-                  <Link to="/add-product" className="navbar-item">
-                    Add Product
-                  </Link>
-                )}
-                <Link to="/cart" className="navbar-item">
-                  Cart
-                  <span
-                    className="tag is-primary"
-                    style={{ marginLeft: "5px" }}
-                  >
-                    { Object.keys(this.state.cart).length }
-                  </span>
-                </Link>
-                {!this.state.user ? (
-                  <Link to="/login" className="navbar-item">
-                    Login
-                  </Link>
-                ) : (
-                  <Link to="/" onClick={this.logout} className="navbar-item">
-                    Logout
-                  </Link>
-                )}
+        <Context.Provider
+          value={{
+            ...this.state,
+            removeFromCart: this.removeFromCart,
+            addToCart: this.addToCart,
+            login: this.login,
+            addProduct: this.addProduct,
+            clearCart: this.clearCart,
+            checkout: this.checkout
+          }}
+        >
+          <Router innerRef={this.routerRef}>
+          <div className="App">
+            <nav
+              className="navbar container"
+              role="navigation"
+              aria-label="main navigation"
+            >
+              <div className="navbar-brand">
+                <b className="navbar-item is-size-4 ">mockapp</b>
+                <label
+                  role="button"
+                  className="navbar-burger burger"
+                  aria-label="menu"
+                  aria-expanded="false"
+                  data-target="navbarBasicExample"
+                  onClick={e => {
+                    e.preventDefault();
+                    this.setState({ showMenu: !this.state.showMenu });
+                  }}
+                >
+                  <span aria-hidden="true"></span>
+                  <span aria-hidden="true"></span>
+                  <span aria-hidden="true"></span>
+                </label>
               </div>
-            </nav>
-            <Routes>
-              <Route exact path="/" element={ProductList} />
-              <Route exact path="/login" element={Login} />
-              <Route exact path="/cart" element={Cart} />
-              <Route exact path="/add-product" element={AddProduct} />
-              <Route exact path="/products" element={ProductList} />
-            </Routes>
-          </div>
-        </Router>
-      </Context.Provider>
+                <div className={`navbar-menu ${
+                    this.state.showMenu ? "is-active" : ""
+                  }`}>
+                  <Link to="/products" className="navbar-item">
+                    Products
+                  </Link>
+                  {this.state.user && this.state.user.accessLevel < 1 && (
+                    <Link to="/add-product" className="navbar-item">
+                      Add Product
+                    </Link>
+                  )}
+                  <Link to="/cart" className="navbar-item">
+                    Cart
+                    <span
+                      className="tag is-primary"
+                      style={{ marginLeft: "5px" }}
+                    >
+                      { Object.keys(this.state.cart).length }
+                    </span>
+                  </Link>
+                  {!this.state.user ? (
+                    <Link to="/login" className="navbar-item">
+                      Login
+                    </Link>
+                  ) : (
+                    <Link to="/" onClick={this.logout} className="navbar-item">
+                      Logout
+                    </Link>
+                  )}
+                </div>
+              </nav>
+              <Routes>
+                <Route exact path="/" element={<ProductList/>} />
+                <Route exact path="/login" element={<Login/>} />
+                <Route exact path="/cart" element={<Cart/>} />
+                <Route exact path="/add-product" element={<AddProduct/>} />
+                <Route exact path="/products" element={<ProductList/>} />
+              </Routes>
+            </div>
+          </Router>
+        </Context.Provider>
     );
   }
 }
