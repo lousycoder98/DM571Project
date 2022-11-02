ef7b8ca12631900128f59458799e7dc1cf27e97a
bobby <cxiangyu@gmail.com>
2022-11-02 15:45:16 +0100

    updated register.js

--- mockapp/src/components/Register.js
+++ mockapp/src/components/Register.js
@@ -1,1 +1,37 @@
-hello
+import React, { Component } from "react";
+import withContext from "../withContext";
+import { Navigate } from "react-router-dom";
+import axios from 'axios';
+
+const initState = {
+    email: "",
+    password: "",
+  };
+
+class Register extends  Component {
+    constructor(props) {
+        super(props);
+        this.state = initState;
+    }
+
+    save = async (e) => {
+        e.preventDefault();
+        const {email, password} =this.state;
+
+        if (email && password) {
+            const id = Math.random();
+
+            this.props.context.addUser(
+                {
+                    email,
+                    password,
+                },
+                () => this.setState(initState)
+            );
+            this.setState(
+                { flash: { status: 'is-success', msg: 'User created successfully' }}
+            )
+
+        }
+    }
+}
