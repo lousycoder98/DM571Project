c07b005a1d0871585d4d83eb6e9e44849f13f3c5
bobby <cxiangyu@gmail.com>
2022-11-02 21:10:53 +0100

    updated register

--- mockapp/src/components/Register.js
+++ mockapp/src/components/Register.js
@@ -38,5 +38,33 @@ class Register extends  Component {
         }
     };
 
-    
+    handleChange = e => this.setState({ [e.target.name]: e.target.value, error: "" });
+
+    render(){
+        const { email, password } = this.state;
+
+        return !this.props.context.user ? (
+            <>
+                <div className="hero is-link is-small">
+                    <div className="hero-body container">
+                        <h4 className="title">Create User</h4>
+                    </div>
+                </div>
+                <br />
+                <br />
+                <form onSubmit={this.save}>
+                    <div className="columns is-mobile is-centered">
+                        <div className="column is one-third">
+                            <div className="field">
+                                <label className="label">Email:</label>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </>
+        )
+
+        
+    }
+
 }
