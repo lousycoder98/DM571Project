01df2a72db87d57e3ef2d67434b7c097b93c8569
Khai <khai.ly.dinh@tul.cz>
2022-11-15 13:06:42 +0100

    delete extra files

--- node_modules/json-server-auth/dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Handler } from 'express';
-import { rewriter } from './guards';
-interface MiddlewaresWithRewriter extends Array<Handler> {
-    rewriter: typeof rewriter;
-}
-declare const middlewares: MiddlewaresWithRewriter;
-export = middlewares;
